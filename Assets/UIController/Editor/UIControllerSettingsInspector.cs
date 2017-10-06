de805eb8921a770ade0a9fca8566ca6fce595a8b
johnsoncodehk <johnsoncodehk@gmail.com>
2017-10-07 07:24:24 +0800

    Add Settings Inspector

---
+++ Assets/UIController/Editor/UIControllerSettingsInspector.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace JohnsonCodeHK.UIControllerEditor {
+
+	[CanEditMultipleObjects, CustomEditor(typeof(UIControllerSettings), true)]
+	public class UIControllerSettingsInspector : Editor {
+
+		public override void OnInspectorGUI() {
+			base.OnInspectorGUI();
+			GUILayout.Label("");
+
+			string info = "Version: 2.2.3";
+			info += "\n" + "License: Pro";
+			EditorGUILayout.HelpBox(info, MessageType.Info);
+		}
+	}
+}
