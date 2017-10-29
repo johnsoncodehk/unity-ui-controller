23d88193d4546f3992d27737aafb95c23995eb20
johnsoncodehk <johnsoncodehk@gmail.com>
2017-10-30 04:04:23 +0800

    Fix Setting Inspector version

--- Assets/UIController/Editor/UIControllerSettingsInspector.cs
+++ Assets/UIController/Editor/UIControllerSettingsInspector.cs
@@ -12,7 +12,7 @@ namespace JohnsonCodeHK.UIControllerEditor {
 			GUI.enabled = true;
 			GUILayout.Label("");
 
-			string info = "Version: 2.2.3";
+			string info = "Version: 2.3";
 			info += "\n" + "License: Free";
 			info += "\n" + "";
 			info += "\n" + "This free version already contains most of the features, and the full version of the difference just is can use settings, so if you buy the full version, a greater degree is to give me support!";
