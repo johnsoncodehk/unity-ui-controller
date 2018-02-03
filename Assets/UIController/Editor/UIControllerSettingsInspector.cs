8363e44b7414e0f4971f5ad81c56adef24c00eb5
johnsoncodehk <johnsoncodehk@gmail.com>
2018-02-03 18:44:05 +0800

    Version 2.4, Add AssetInspector(Readme Only)

--- Assets/UIController/Editor/UIControllerSettingsInspector.cs
+++ Assets/UIController/Editor/UIControllerSettingsInspector.cs
@@ -12,7 +12,7 @@ namespace JohnsonCodeHK.UIControllerEditor {
 			GUI.enabled = true;
 			GUILayout.Label("");
 
-			string info = "Version: 2.3.1";
+			string info = "Version: 2.4";
 			info += "\n" + "License: Free";
 			info += "\n" + "";
 			info += "\n" + "This free version already contains most of the features, and the full version of the difference just is can use settings, so if you buy the full version, a greater degree is to give me support!";
